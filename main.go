7239cb57b0c8e3515f7de6d452ce6aba8513661a
Jacob Simpson <jacob.simpson@gmail.com>
2021-01-20 21:54:50 -0800

    Update prompt to use a readline library.
    Corresponds roughly to the second half of chapter 4 in buildyourownlisp.
    
    http://www.buildyourownlisp.com/chapter4_interactive_prompt

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"log"
+
+	"github.com/peterh/liner"
 )
 
 func main() {
-	/* Print Version and Exit Information */
+	line := liner.NewLiner()
+	defer line.Close()
+
+	line.SetCtrlCAborts(true)
+
+	// Print Version and Exit Information.
 	fmt.Println("Lispy Version 0.0.0.0.1")
 	fmt.Println("Press Ctrl+c to Exit\n")
 
-	/* In a never ending loop */
+	// In a never ending loop.
 	for {
-		/* Output our prompt */
-		fmt.Printf("lispy> ")
-
-		/* Read a line of user input of maximum size 2048 */
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
-		/* Echo input back to user */
-		fmt.Printf("No you're a %s\n", input)
+		// Output our prompt and read a line of user input.
+		if input, err := line.Prompt("lispy> "); err == nil {
+			// Echo input back to user
+			fmt.Printf("No you're a %s\n", input)
+			line.AppendHistory(input)
+		} else if err == liner.ErrPromptAborted {
+			break
+		} else {
+			log.Print("Error reading line: ", err)
+		}
 	}
 }
